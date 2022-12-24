1013eed76cc797ff900f7e7b6dd138c5b7b6f5da
anisul islam <anisulislam980340@gmail.com>
2022-12-25 01:01:56 +0600

    setup mongodb

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const app = express()
 const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
+require('dotenv').config()
+
+
+//setup mongodb
+// dbuser name: foodService
+// dbuser password: Nfjkb8oebzOepDG5
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b986vqn.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get('/', (req, res)=>{
     res.send('server setup sucessfully')

f114313329b486100932d868f390a13dccacc757
anisul islam <anisulislam980340@gmail.com>
2022-12-25 09:50:53 +0600

    completely setup mongodb database and server side

--- index.js
+++ index.js
@@ -16,11 +16,21 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b986vqn.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run(){
+  try{
+    const foodCollection = client.db("foodServices").collection("foods")
+    
+    
+
+
+  }
+  finally{
+
+  }
+  
+
+}
+run().catch(error => console.log(error))
 
 app.get('/', (req, res)=>{
     res.send('server setup sucessfully')

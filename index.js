c4ec0971367deef9467575ebcc33237c15c985c5
anisul islam <anisulislam980340@gmail.com>
2022-12-27 19:11:37 +0600

    store review in the review collection

--- index.js
+++ index.js
@@ -16,44 +16,55 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b986vqn.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-  try{
+async function run() {
+  try {
     const foodCollection = client.db("foodServices").collection("foods")
-    app.get('/services', async(req, res)=>{
+
+    const foodReviewCollection = client.db("foodServices").collection("reviews")
+    app.get('/services', async (req, res) => {
       const query = {}
       const cursor = foodCollection.find(query).limit(3)
       const result = await cursor.toArray()
       res.send(result)
-      console.log(result)
+
 
     })
-    app.get('/servicesDetails', async(req,res)=>{
+    app.get('/servicesDetails', async (req, res) => {
       const query = {}
       const cursor = foodCollection.find(query)
       const result = await cursor.toArray()
       res.send(result)
-      
+
     })
-    
-    app.get('/viewDetails/:id', async(req, res)=>{
+
+
+    app.get('/viewDetails/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)}
+      const query = { _id: ObjectId(id) }
       const result = await foodCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.post('/reviews', async (req, res) => {
+      const info = req.body;
+      console.log(info)
+      const result = await foodReviewCollection.insertOne(info)
+      console.log(result)
       res.send(result)
     })
 
 
   }
-  finally{
+  finally {
 
   }
-  
+
 
 }
 run().catch(error => console.log(error))
 
-app.get('/', (req, res)=>{
-    res.send('server setup sucessfully')
+app.get('/', (req, res) => {
+  res.send('server setup sucessfully')
 
 })
-app.listen(port , ()=> console.log(`server setup completely ${port}`))
+app.listen(port, () => console.log(`server setup completely ${port}`))
